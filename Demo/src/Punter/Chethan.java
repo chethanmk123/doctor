b4dd84e4ba2809af38504fbb15718e922aa2c36d
Chethan M K <Chethan M K@LAPTOP-FQFCD50K>
2023-02-10 18:30:14 +0530

    review

---
+++ Demo/src/Punter/Chethan.java
@@ -0,0 +1,17 @@
+package Punter;
+
+public class Chethan 
+{
+	public int hashcode()
+	{
+		return 12345;
+		
+	}
+	public static void main(String[] args) 
+	{
+		Chethan C= new Chethan();
+		System.out.println(C.hashcode());
+		
+	}
+
+}
---
+++ Demo/src/Punter/Demo2.java
@@ -0,0 +1,16 @@
+package Punter;
+
+public class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		Demo2  D2= new Demo2 ();
+		System.out.println(D2);
+		Demo2 D3=new Demo2();
+		System.out.println(D3);
+		System.out.println(D2.equals(D3));
+		
+	}
+
+}
+
